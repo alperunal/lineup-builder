376869856fa8a1830897f28deeb9028d81af2b37
Alper Unal <alper.91@hotmail.com>
2020-07-14 22:03:16 +0200

    lambda func fixes

--- functions/getLineUp.js
+++ functions/getLineUp.js
@@ -1,110 +1,111 @@
-/* eslint-disable */
-const Faker = require('faker');
+/* eslint-disable no-multi-spaces */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const faker = require('faker');
 
 function getFormation(formation) {
     switch (formation) {
-        case '4-2-3-1':
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 350 }, // DR
-                { x: 200, y: 365 }, // DCR
-                { x: 120, y: 365 }, // DCL
-                { x: 45, y: 350 },  // DL
-                { x: 295, y: 190 }, // RM
-                { x: 210, y: 250 }, // MCR
-                { x: 105, y: 250 }, // MCL
-                { x: 25, y: 190 },  // ML
-                { x: 160, y: 140 }, // AMC
-                { x: 160, y: 60 },  // ST
-            ];
-        case '4-4-2':
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 350 }, // DR
-                { x: 200, y: 365 }, // DCR
-                { x: 120, y: 365 }, // DCL
-                { x: 45, y: 350 },  // DL
-                { x: 295, y: 230 }, // RM
-                { x: 210, y: 250 }, // MCR
-                { x: 105, y: 250 }, // MCL
-                { x: 25, y: 230 },  // ML
-                { x: 160, y: 140 }, // AMC
-                { x: 160, y: 60 },  // ST
-            ];
-        case '4-4-1-1':
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 350 }, // DR
-                { x: 200, y: 365 }, // DCR
-                { x: 120, y: 365 }, // DCL
-                { x: 45, y: 350 },  // DL
-                { x: 295, y: 230 }, // RM
-                { x: 210, y: 250 }, // MCR
-                { x: 105, y: 250 }, // MCL
-                { x: 25, y: 230 },  // ML
-                { x: 160, y: 140 }, // AMC
-                { x: 160, y: 60 },  // ST
-            ];
-        case '4-3-3':
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 350 }, // DR
-                { x: 200, y: 365 }, // DCR
-                { x: 120, y: 365 }, // DCL
-                { x: 45, y: 350 },  // DL
-                { x: 295, y: 190 }, // RM
-                { x: 210, y: 250 }, // MCR
-                { x: 105, y: 250 }, // MCL
-                { x: 25, y: 190 },  // ML
-                { x: 160, y: 140 }, // AMC
-                { x: 160, y: 60 },  // ST
-            ];
-        case '3-5-2':
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 260 }, // WBR
-                { x: 220, y: 365 }, // DCR
-                { x: 160, y: 365 }, // DC
-                { x: 100, y: 365 }, // DCL
-                { x: 45, y: 260 },  // WBL
-                { x: 210, y: 250 }, // MCR
-                { x: 25, y: 190 },  // ML
-                { x: 105, y: 250 }, // MCL
-                { x: 120, y: 60 }, // STL
-                { x: 200, y: 60 },  // STR
-            ];
-        default:
-            return [
-                { x: 160, y: 435 }, // GK
-                { x: 275, y: 350 }, // DR
-                { x: 200, y: 365 }, // DCR
-                { x: 120, y: 365 }, // DCL
-                { x: 45, y: 350 },  // DL
-                { x: 295, y: 190 }, // RM
-                { x: 210, y: 250 }, // MCR
-                { x: 105, y: 250 }, // MCL
-                { x: 25, y: 190 },  // ML
-                { x: 160, y: 140 }, // AMC
-                { x: 160, y: 60 },  // ST
-            ];
+    case '4-2-3-1':
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 350 }, // DR
+            { x: 200, y: 365 }, // DCR
+            { x: 120, y: 365 }, // DCL
+            { x: 45, y: 350 },  // DL
+            { x: 295, y: 190 }, // RM
+            { x: 210, y: 250 }, // MCR
+            { x: 105, y: 250 }, // MCL
+            { x: 25, y: 190 },  // ML
+            { x: 160, y: 140 }, // AMC
+            { x: 160, y: 60 },  // ST
+        ];
+    case '4-4-2':
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 350 }, // DR
+            { x: 200, y: 365 }, // DCR
+            { x: 120, y: 365 }, // DCL
+            { x: 45, y: 350 },  // DL
+            { x: 295, y: 230 }, // RM
+            { x: 210, y: 250 }, // MCR
+            { x: 105, y: 250 }, // MCL
+            { x: 25, y: 230 },  // ML
+            { x: 160, y: 140 }, // AMC
+            { x: 160, y: 60 },  // ST
+        ];
+    case '4-4-1-1':
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 350 }, // DR
+            { x: 200, y: 365 }, // DCR
+            { x: 120, y: 365 }, // DCL
+            { x: 45, y: 350 },  // DL
+            { x: 295, y: 230 }, // RM
+            { x: 210, y: 250 }, // MCR
+            { x: 105, y: 250 }, // MCL
+            { x: 25, y: 230 },  // ML
+            { x: 160, y: 140 }, // AMC
+            { x: 160, y: 60 },  // ST
+        ];
+    case '4-3-3':
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 350 }, // DR
+            { x: 200, y: 365 }, // DCR
+            { x: 120, y: 365 }, // DCL
+            { x: 45, y: 350 },  // DL
+            { x: 295, y: 190 }, // RM
+            { x: 210, y: 250 }, // MCR
+            { x: 105, y: 250 }, // MCL
+            { x: 25, y: 190 },  // ML
+            { x: 160, y: 140 }, // AMC
+            { x: 160, y: 60 },  // ST
+        ];
+    case '3-5-2':
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 260 }, // WBR
+            { x: 220, y: 365 }, // DCR
+            { x: 160, y: 365 }, // DC
+            { x: 100, y: 365 }, // DCL
+            { x: 45, y: 260 },  // WBL
+            { x: 210, y: 250 }, // MCR
+            { x: 25, y: 190 },  // ML
+            { x: 105, y: 250 }, // MCL
+            { x: 120, y: 60 }, // STL
+            { x: 200, y: 60 },  // STR
+        ];
+    default:
+        return [
+            { x: 160, y: 435 }, // GK
+            { x: 275, y: 350 }, // DR
+            { x: 200, y: 365 }, // DCR
+            { x: 120, y: 365 }, // DCL
+            { x: 45, y: 350 },  // DL
+            { x: 295, y: 190 }, // RM
+            { x: 210, y: 250 }, // MCR
+            { x: 105, y: 250 }, // MCL
+            { x: 25, y: 190 },  // ML
+            { x: 160, y: 140 }, // AMC
+            { x: 160, y: 60 },  // ST
+        ];
     }
 }
 
 function generate(type) {
     switch (type) {
     case 'name':
-        return `${Faker.name.lastName()}`;
+        return `${faker.name.lastName()}`;
     case 'team':
-        return `${Faker.company.companySuffix()}`;
+        return `${faker.company.companySuffix()}`;
     case 'number':
-        return Faker.random
+        return faker.random
             .number({
                 min: 1,
                 max: 25,
             })
             .toString();
     default:
-        return Faker.random.number(99).toString();
+        return faker.random.number(99).toString();
     }
 }
 
@@ -113,7 +114,7 @@ function generateData() {
     const players = [];
     for (let i = 0; i < 11; i += 1) {
         const player = {
-            id: Faker.random.alphaNumeric(5),
+            id: faker.random.alphaNumeric(5),
             name: generate('name'),
             num: generate('number'),
             position: positions.pop() || { x: 0, y: 0 },
