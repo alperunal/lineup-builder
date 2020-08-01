a236680dc9d47e60022e7dad293c3ce600f64ec8
Alper Unal <alper.91@hotmail.com>
2020-08-01 11:02:27 +0200

    voet-22 variables init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render, hydrate } from 'react-dom';
 import App from './containers/App/App';
-import './styles.scss';
+import './styles/style.scss';
 
 const rootElement = document.getElementById('root');
 if (rootElement?.hasChildNodes()) {
