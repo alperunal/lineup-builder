edb3fa9b08ede24870bce3e4919a4c3d540bea2c
alperunal <unalmehmetalper@gmail.com>
2020-08-05 01:38:00 +0200

    mobile design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import cn from 'classnames';
 import imageSrc from 'assets/images/logo.png';
 import { Link } from 'react-router-dom';
 import StoreContext from 'store';
 import { Container, Switch } from 'components/UI';
+import HamburgerIcon from 'assets/icons/hamburger-menu.svg';
 import './Header.module.scss';
 
 const Header: React.FC = () => {
@@ -11,6 +13,47 @@ const Header: React.FC = () => {
     const {
         language, changeLanguage, theme, changeTheme,
     } = context;
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const linkList = (
+        <ul className="nav__list">
+            <li className="nav__list-item">
+                <Link to="/">
+                    <FormattedMessage
+                        id="header.home"
+                        description="Homepage"
+                        defaultMessage="Home"
+                    />
+                </Link>
+            </li>
+            <li className="nav__list-item">
+                <Link to="/lineup">
+                    <FormattedMessage
+                        id="header.lineup"
+                        description="Lineup Builder"
+                        defaultMessage="Lineup Builder"
+                    />
+                </Link>
+            </li>
+            <li className="nav__list-item">
+                <select
+                    value={language}
+                    onChange={(event: React.ChangeEvent<HTMLSelectElement>) => changeLanguage(event.target.value)}
+                >
+                    <option value="en">EN</option>
+                    <option value="tr">TR</option>
+                </select>
+            </li>
+            <li className="nav__list-item">
+                <Switch
+                    id="theme-selector"
+                    checked={theme === 'dark'}
+                    size="small"
+                    handleChange={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
+                />
+            </li>
+        </ul>
+    );
 
     return (
         <header className="header">
@@ -21,44 +64,33 @@ const Header: React.FC = () => {
                             <img className="header__logo" src={imageSrc} alt="Voety" />
                         </Link>
                     </div>
+                    <div>
+                        <button
+                            type="button"
+                            onClick={() => setToggleMenu(!toggleMenu)}
+                            className="hamburger-button"
+                        >
+                            <img
+                                src={HamburgerIcon}
+                                alt="Menu"
+                                className={
+                                    cn(
+                                        'hamburger-menu',
+                                        {
+                                            'hamburger-menu--dark': theme === 'dark',
+                                        },
+                                    )
+                                }
+                            />
+                        </button>
+                        {toggleMenu ? (
+                            <nav className="hamburger-nav">
+                                {linkList}
+                            </nav>
+                        ) : null}
+                    </div>
                     <nav className="nav">
-                        <ul className="nav__list">
-                            <li className="nav__list-item">
-                                <Link to="/">
-                                    <FormattedMessage
-                                        id="header.home"
-                                        description="Homepage"
-                                        defaultMessage="Home"
-                                    />
-                                </Link>
-                            </li>
-                            <li className="nav__list-item">
-                                <Link to="/lineup">
-                                    <FormattedMessage
-                                        id="header.lineup"
-                                        description="Lineup Builder"
-                                        defaultMessage="Lineup Builder"
-                                    />
-                                </Link>
-                            </li>
-                            <li className="nav__list-item">
-                                <select
-                                    value={language}
-                                    onChange={(event: React.ChangeEvent<HTMLSelectElement>) => changeLanguage(event.target.value)}
-                                >
-                                    <option value="en">EN</option>
-                                    <option value="tr">TR</option>
-                                </select>
-                            </li>
-                            <li className="nav__list-item">
-                                <Switch
-                                    id="theme-selector"
-                                    checked={theme === 'dark'}
-                                    size="small"
-                                    handleChange={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
-                                />
-                            </li>
-                        </ul>
+                        {linkList}
                     </nav>
                 </div>
             </Container>
